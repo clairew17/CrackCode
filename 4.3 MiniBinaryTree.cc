c8ca30799859702ff531f7f80468c9385783a758
claire wang <hellowang@foxmail.com>
2015-12-16 00:21:18 -0500

    min height btree

--- 4.3 MiniBinaryTree.cc
+++ 4.3 MiniBinaryTree.cc
@@ -1,6 +1,6 @@
 //MiniBinaryTree.cc
 
-Given a sorted (increasing order) array, write an algorithm to create a binary tree with minimal height.
+//Given a sorted (increasing order) array, write an algorithm to create a binary tree with minimal height.
 
 
 TreeNode* MiniBinaryTree(vector<int> nums){
